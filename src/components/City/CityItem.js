61ffb4c8a6749d4252949fc4e046e52a2e5741f7
sevinn21 <sevinn20@gmail.com>
2020-12-10 21:42:42 +0300

    create-city-component

---
+++ src/components/City/CityItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CardContent, CardTitle, CardWrapper } from '../../styles/City.styles'
+
+const CityItem = ({name,onShowDetail}) => {
+    return (
+       <CardWrapper
+       onClick={() => {
+        onShowDetail && onShowDetail(name);
+      }}>
+       <CardContent >
+       <CardTitle>{name}</CardTitle>
+       </CardContent>
+       </CardWrapper>
+    )
+}
+
+export default CityItem
---
+++ src/components/City/Citys.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import { Wrapper, WrapperTitle, WrapperTitleDiv} from '../../styles/City.styles'
+import {  getCitys } from '../../services/CitysApi';
+import CityItem from './CityItem';
+import {useHistory} from 'react-router-dom';
+const Citys = () => {
+    
+    const history = useHistory();
+    const [citys, setCitys] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+       const init = async () => {
+           try {
+               const {data} = await getCitys();
+               setCitys(data);
+           } catch(err) {
+               setError(err)
+           }      
+       };
+       init();
+    }, [])
+
+    const handleShowDetailClick = (name) =>  {(
+     history.push(`/city/${name}`)
+    )};
+   
+    return (
+        <>
+        <WrapperTitleDiv>
+        <WrapperTitle >Şehirler</WrapperTitle>
+        </WrapperTitleDiv>
+      
+       <Wrapper>   
+           { citys.map((item)=> (
+               <CityItem
+               data-testId ="contact-name"
+               key={item.id}
+               name={item.name}
+               onShowDetail={handleShowDetailClick}
+               />
+           ))
+           }
+       </Wrapper>
+       </>
+    )
+}
+
+export default Citys
